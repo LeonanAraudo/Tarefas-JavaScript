89ca6b837fa0772f431f39f2538bf1458498913d
LeonanAraudo <LeonanArajoReis@outlook.com>
2024-02-27 20:52:01 -0300

    att

--- anos luz/index2.js
+++ anos luz/index2.js
@@ -1,21 +1,24 @@
 let quaqui = prompt("Digite a distancia em anos luz")
 let option = prompt("selecione a opção de conversão \n 1-Parsec \n 2-Unidade Aatronomica \n 3-Quilômetro")
 
+
+let nome
+let calculado
 let result = option
 
 switch(result){
     case "1":
-        let pac = (quaqui * 0.306601)
-        alert("Distanciância em anos luz: " + quaqui + "\nA distancia em Parsec é: " + pac)
-        break
+        nome = "Parsec"
+        calculado = (quaqui * 0.306601)
     case "2":
-        let au = (quaqui * 632441.1)
-        alert("Distanciância em anos luz: " + quaqui + "\nA distância em Unidade Astronômica é:" + au)
+        nome = "Unidade Astronômica"
+        calculado = (quaqui * 632441.1)
         break
         case "3":
-            let km = (quaqui * (Math.pow(10,12) * 9.5) )
-            alert("Distanciância em anos luz: " + quaqui + "\nA distância em Quilômetros é: " + km)
+            nome = "Quilômetro"
+            calculado = (quaqui * (Math.pow(10,12) * 9.5) )
             break
         default:
             alert("Distanciância em anos luz: " + quaqui + "\n Opção de conversão: Não identificada")
 }
+alert("Distanciância em anos luz: " + quaqui + "\nA distância em " + nome + ": " + calculado)
